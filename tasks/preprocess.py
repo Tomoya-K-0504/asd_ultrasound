5cd9498f84ea8687a9c1e202fedb5102e72a381e
Tomoya Koike <makeffort134@gmail.com>
2020-06-01 22:39:21 +0900

    Split to frames added

--- tasks/preprocess.py
+++ tasks/preprocess.py
@@ -9,37 +9,47 @@ from pathlib import Path
 from typing import Dict
 from typing import List
 
-import mlflow
 import numpy as np
 import pandas as pd
-from aggregate import aggregate
-from joblib import Parallel, delayed
-from ml.src.dataset import CSVDataSet
-from ml.tasks.base_experiment import typical_train, base_expt_args, typical_experiment
-from preprocess import preprocess
-from scipy.stats import stats
-from ml.utils.notify_slack import notify_slack
-from ml.utils.utils import dump_dict
-from sklearn.metrics import balanced_accuracy_score, confusion_matrix
+from tqdm import tqdm
 
 
 def expt_args(parser):
-    parser = base_expt_args(parser)
     prep_parser = parser.add_argument_group("ASD ultrasound")
     prep_parser.add_argument('--pool-size', default=5, type=int)
-    prep_parser.add_argument('--split-size', default=100, type=int)
+    prep_parser.add_argument('--win-length', default=300, type=int)
+    prep_parser.add_argument('--win-stride', default=150, type=int)
 
     return parser
 
 
-def preprocess():
+def preprocess(expt_conf):
     """
-    Spectrogram(1500 x 30000) -> avg pool (5 x 1) -> Spectrogram (300 x 30000) -> Spectrogram (300 x 300) x 100
-
+    Spectrogram (300 x 3000) -> Split -> Spectrogram (300 x 200) x 18
     :return:
     """
-    pass
+    input_dir = Path(__file__).parent.resolve() / 'input'
+    output_dir = Path(__file__).parent.resolve() / 'input' / 'split'
+    output_dir.mkdir(exist_ok=True)
+    label_df = pd.read_excel(input_dir / 'P08_12_USV_15qDup_MouseInfo.xlsx')
+    label_kind = {'pat': 1, 'WT': 0}
+    subj2label = dict(zip(label_df['MouseID'], label_df['Genotype']))
+
+    spect_list = sorted((input_dir / 'processed').iterdir())
+
+    for spect_path in tqdm(spect_list):
+        spect = np.load(spect_path)
+        label = subj2label[spect_path.name.split('_')[0]]
+
+        n_frames = (spect.shape[1] - expt_conf['win_length']) // expt_conf['win_stride'] + 1
+        for i_frame in n_frames:
+            s_idx = i_frame * expt_conf['win_stride']
+            frame = spect[:, s_idx:s_idx + expt_conf['win_length']]
+            file_name = f"{label}_{spect_path.name.split('.')[0]}_{i_frame}.npy"
+            np.save(frame, output_dir / file_name)
 
 
 if __name__ == '__main__':
-    preprocess()
+    parser = argparse.ArgumentParser(description='train arguments')
+    expt_conf = vars(expt_args(parser).parse_args())
+    preprocess(expt_conf)
